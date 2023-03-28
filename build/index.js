a35ab3bcd9980b8183ff3fbd7a12af6e6c5af8bd
dibkb <dibas9110@gmail.com>
2023-03-28 22:59:41 +0530

    ts

--- build/index.js
+++ build/index.js
@@ -1,1 +1,4 @@
 "use strict";
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissor = document.getElementById("scissor");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissor = document.getElementById("scissor");
