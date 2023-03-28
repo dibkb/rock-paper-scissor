fcb10991f07bdd302fff795076260d427b902d5e
dibkb <dibas9110@gmail.com>
2023-03-28 23:15:26 +0530

    select

--- build/index.js
+++ build/index.js
@@ -2,3 +2,20 @@
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissor = document.getElementById("scissor");
+const body = document.querySelector("body");
+const heading = document.querySelector("p.heading");
+// add eventlisterns------------------
+let selectedItem;
+rock.addEventListener("click", handleClick);
+paper.addEventListener("click", handleClick);
+scissor.addEventListener("click", handleClick);
+function handleClick(ev) {
+    selectedItem = this;
+    body.removeChild(body.children[0]);
+    body.textContent = "";
+    console.log(selectedItem);
+    //   body?.removeChild(rock);
+    //   body?.removeChild(paper);
+    //   body?.removeChild(scissor);
+    //   body?.removeChild(heading);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissor = document.getElementById("scissor");
+const rock = document.getElementById("rock") as HTMLMediaElement;
+const paper = document.getElementById("paper") as HTMLMediaElement;
+const scissor = document.getElementById("scissor") as HTMLMediaElement;
+const body = document.querySelector("body") as HTMLBodyElement;
+const heading = document.querySelector("p.heading") as HTMLParagraphElement;
+// add eventlisterns------------------
+let selectedItem: HTMLMediaElement;
+rock.addEventListener("click", handleClick);
+paper.addEventListener("click", handleClick);
+scissor.addEventListener("click", handleClick);
+function handleClick(this: HTMLMediaElement, ev: MouseEvent): void {
+  selectedItem = this;
+  body.removeChild(body.children[0]);
+  body.textContent = "";
+  console.log(selectedItem);
+  //   body?.removeChild(rock);
+  //   body?.removeChild(paper);
+  //   body?.removeChild(scissor);
+  //   body?.removeChild(heading);
+}
